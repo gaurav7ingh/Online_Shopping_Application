74601b3b6853e9c7ff92eaa82f8df94a313eeff0
gaurav7ingh <gauravsingh99626@gmail.com>
2022-11-09 20:06:04 +0530

    Controller classes are created

--- src/main/java/com/shopping/controller/AddressController.java
+++ src/main/java/com/shopping/controller/AddressController.java
@@ -9,13 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
-public class MyController {
+public class AddressController {
 
 
-	// just for tasting the swagger
-	@GetMapping("/{name}")
-	public ResponseEntity<String> sayHelloTo(@PathVariable String name) {
-		return new ResponseEntity<>("Hello " + name, HttpStatus.OK);
-	}
+//	// just for tasting the swagger
+//	@GetMapping("/{name}")
+//	public ResponseEntity<String> sayHelloTo(@PathVariable String name) {
+//		return new ResponseEntity<>("Hello " + name, HttpStatus.OK);
+//	}
 
 }
---
+++ src/main/java/com/shopping/model/ShoppingError.java
@@ -0,0 +1,20 @@
+package com.shopping.model;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ShoppingError {
+
+	private LocalDateTime timestamp;
+	private String message;
+	private String description;
+	private String path;
+
+}
