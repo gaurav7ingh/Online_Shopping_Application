56678d990cf2c669affdb115ef81d007d3bc4891
gaurav7ingh <gauravsingh99626@gmail.com>
2022-11-09 20:40:48 +0530

    FIXED : main uris fixed

--- src/main/java/com/shopping/controller/AddressController.java
+++ src/main/java/com/shopping/controller/AddressController.java
@@ -1,21 +1,10 @@
 package com.shopping.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/addresses")
 public class AddressController {
-
-
-//	// just for tasting the swagger
-//	@GetMapping("/{name}")
-//	public ResponseEntity<String> sayHelloTo(@PathVariable String name) {
-//		return new ResponseEntity<>("Hello " + name, HttpStatus.OK);
-//	}
 
 }
